d8401702410e0ab084cbdeb3f5d85d06223b993d
Kevin Foss <kevin.foss@gmail.com>
2017-12-02 16:24:31 -0500

    Move tomltest here, this may go away eventually.

---
+++ tomltest/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	//"os"
+	"log"
+		"bytes"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/spf13/viper"
+	"cursheet/shared"
+)
+
+func main() {
+	
+	var c cursheet.Config
+	var col cursheet.Column
+
+	c.Title = "two one"
+	col.Name = "id"
+	col.Ctype = "string"
+
+	c.Cols = append(c.Cols, col)
+	col.Name = "name"
+	col.Ctype = "string"
+	c.Cols = append(c.Cols, col)
+	//fmt.Println(c)
+
+	buf := new(bytes.Buffer)
+	if err := toml.NewEncoder(buf).Encode(c); err != nil {
+		log.Fatal(err)
+	} 
+
+
+	viper.SetConfigType("toml")
+	viper.ReadConfig(buf)
+
+	var z cursheet.Config
+	err := viper.Unmarshal(&z)
+		if err != nil {
+			panic(err)
+		}
+
+	fmt.Println(len(z.Cols))
+	fmt.Println(z.Cols[1].Name)
+
+}
